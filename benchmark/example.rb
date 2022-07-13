eff7585df2c0ba7a02ae110471174eac9ffeeddc
Danila Kravchenko <88050202+KravchaDev@users.noreply.github.com>
2022-07-13 09:33:41 +0800

    code style

--- benchmark/example.rb
+++ benchmark/example.rb
@@ -1,8 +1,8 @@
 # coding: utf-8
 # frozen_string_literal: true
 
-require('benchmark/ips')
-require_relative('../lib/my_new_library')
+require 'benchmark/ips'
+require_relative '../lib/my_new_library'
 
 Benchmark.ips do |x|
   x.report('MyNewLibrary.something') { MyNewLibrary.something }
